ca66e7153b18e14e8d923646db418b6735cd9e57
Lucas Abbade <lucasabbade@Lucass-MacBook-Pro.local>
2018-09-06 15:04:28 -0300

    updates

--- receipt.cpp
+++ receipt.cpp
@@ -21,5 +21,35 @@ using eosio::print;
 using eosio::name;
 
 class receipt : public eosio::contract {
-   
+  public:
+    receipt(account_name self)
+        : contract(self), _accounts(_self, _self) {}
+
+    void insert(account_name from, uint64_t DescReceita, uint64_t NumExercicio, uint64_t NumReceita, uint64_t VlPrevisto, uint64_t VlRealizado)
+    {
+        require_auth(from);
+
+        data d(from, DescReceita, NumExercicio, NumReceita, VlPrevisto, VlRealizado);
+        // enfia d na blockchain
+    }
+
+  private:
+    struct data
+    {
+        account_name institution;       // the institution that inserted the data
+        uint64_t DescReceita;
+        uint64_t NumExercicio;
+        uint64_t NumReceita;
+        uint64_t VlPrevisto;
+        uint64_t VlRealizado;
+
+        data(account_name from, uint64_t dr, uint64_t ne, uint64_t nr, uint64_t vlp, uint64_t vlr)
+            : institution(from), 
+              DescReceita(dr), 
+              NumExercicio(ne), 
+              NumReceita(nr),  
+              VlPrevisto(vlp),
+              VlRealizado(vlr)
+        {}
+    };
 }

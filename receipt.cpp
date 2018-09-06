4d74ddd45a4b9524b7f2dd8f47c28a5097392f77
Alisson Morais <alissonfpmorais@gmail.com>
2018-09-06 12:38:24 -0300

    add receipt contract

---
+++ receipt.cpp
@@ -0,0 +1,25 @@
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
+#include <utility>
+#include <vector>
+#include <string>
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/time.hpp>
+#include <eosiolib/asset.hpp>
+#include <eosiolib/contract.hpp>
+#include <eosiolib/crypto.h>
+
+using eosio::key256;
+using eosio::indexed_by;
+using eosio::const_mem_fun;
+using eosio::asset;
+using eosio::permission_level;
+using eosio::action;
+using eosio::print;
+using eosio::name;
+
+class receipt : public eosio::contract {
+   
+}

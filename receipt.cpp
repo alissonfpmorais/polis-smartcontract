271a7d1c8a458f83c3aa534667deacb6cd249b09
Alisson Morais <alissonfpmorais@gmail.com>
2018-09-06 16:07:09 -0300

    first attempt

--- receipt.cpp
+++ receipt.cpp
@@ -21,35 +21,44 @@ using eosio::print;
 using eosio::name;
 
 class receipt : public eosio::contract {
-  public:
-    receipt(account_name self)
-        : contract(self), _accounts(_self, _self) {}
-
-    void insert(account_name from, uint64_t DescReceita, uint64_t NumExercicio, uint64_t NumReceita, uint64_t VlPrevisto, uint64_t VlRealizado)
-    {
-        require_auth(from);
-
-        data d(from, DescReceita, NumExercicio, NumReceita, VlPrevisto, VlRealizado);
-        // enfia d na blockchain
-    }
-
-  private:
-    struct data
-    {
-        account_name institution;       // the institution that inserted the data
-        uint64_t DescReceita;
-        uint64_t NumExercicio;
-        uint64_t NumReceita;
-        uint64_t VlPrevisto;
-        uint64_t VlRealizado;
-
-        data(account_name from, uint64_t dr, uint64_t ne, uint64_t nr, uint64_t vlp, uint64_t vlr)
-            : institution(from), 
-              DescReceita(dr), 
-              NumExercicio(ne), 
-              NumReceita(nr),  
-              VlPrevisto(vlp),
-              VlRealizado(vlr)
+    public:
+        receipt(account_name self)
+        :contract(self),
+        invoices(_self, _self)
         {}
-    };
+
+        void insert(account_name from, uint64_t DescReceita, uint64_t NumExercicio, uint64_t NumReceita, uint64_t VlPrevisto, uint64_t VlRealizado)
+        {
+            require_auth(from);
+            
+            auto new_invoice_itr = invoices.emplace(_self, [&](auto& invoice) {
+                invoice.id = invoices.available_primary_key();
+                invoice.institution = from;
+                invoice.DescReceita = DescReceita;
+                invoice.NumExercicio = NumExercicio;
+                invoice.NumReceita = NumReceita;
+                invoice.VlPrevisto = VlPrevisto;
+                invoice.VlRealizado = VlRealizado;
+            });
+        }
+
+    private:
+        struct invoices
+        {
+            uint64_t id;
+            account_name institution;       // the institution that inserted the data
+            uint64_t DescReceita;
+            uint64_t NumExercicio;
+            uint64_t NumReceita;
+            uint64_t VlPrevisto;
+            uint64_t VlRealizado;
+
+            EOSLIB_SERIALIZE(invoice, (DescReceita), (NumExercicio), (NumReceita), (VlPrevisto), (VlRealizado))
+        };
+
+        typedef eosio::multi_index<N(invoice), invoice> invoce_index;
+
+        invoice_index invoices;
 }
+
+EOSIO_ABI(receipt, (insert))
